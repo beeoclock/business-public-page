e66ff8d66091edcc77f4184803dd392305590004
Karba <ivan.karbashevskyi@gmail.com>
2024-10-01 19:13:03 +0200

    Init project, add: tailwind, angular i18n, eslint, prettier, environment, packages to build code, ngxs.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideClientHydration(),
+		provideStore(
+			[],
+			withNgxsReduxDevtoolsPlugin(),
+			withNgxsLoggerPlugin(),
+			withNgxsStoragePlugin({
+				keys: []
+			})
+		),
+	],
 };
