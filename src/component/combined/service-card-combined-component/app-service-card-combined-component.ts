e503f9c325b506a9776b4b0ac84033d0dc9431e9
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2024-10-19 17:54:11 +0200

    i18n

--- src/component/combined/service-card-combined-component/app-service-card-combined-component.ts
+++ src/component/combined/service-card-combined-component/app-service-card-combined-component.ts
@@ -1,22 +1,26 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { AppServiceCardDumpComponent } from '@src/component/dump/service-card-dump-component/app-service-card-dump-component';
+import { AppServiceCounterDumpComponent } from '@src/component/dump/service-card-dump-component/service-counter/app-service-counter-dump-component';
 
 @Component({
   selector: 'app-service-card-combined-component',
   standalone: true,
-  imports: [NgFor, AppServiceCardDumpComponent],
-  template: `<app-service-card-dump-component *ngFor="let property of properties " [properties]="property" />`,
+  imports: [NgFor, AppServiceCardDumpComponent, AppServiceCounterDumpComponent],
+  template: `
+    <div *ngFor="let property of properties">
+      <app-service-card-dump-component [properties]="property"></app-service-card-dump-component>
+    </div>
+  `,
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class AppServiceCardCombinedComponent {
   public readonly properties: {
-src?: string;
-title: string;
-price: string;
-duration: string;
+    src?: string;
+    title: string;
+    price: string;
+    duration: string;
   }[] = [
     {
       src: 'https://images.unsplash.com/photo-1587614203976-365c74645e83?q=80&w=480&h=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -32,15 +36,15 @@ duration: string;
     },
     {
       src: 'https://beeoclock.biz/assets/examples/business-2.jpg',
-      title: 'coloring',
+      title: 'Coloring',
       price: '400 PLN',
       duration: '2 godz 30 min',
     },
     {
       src: 'http://localhost:4201/assets/src.PNG',
-      title: 'keretin straightening of hair',
+      title: 'Keratine Straightening of Hair',
       price: '80 PLN',
       duration: '30 min',
-    }
+    },
   ];
 }
